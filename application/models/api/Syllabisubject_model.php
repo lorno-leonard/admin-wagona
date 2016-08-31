78ffb97200f0c53b0c8640314b352deb91f15b13
Leonard Laput <online.lorno@gmail.com>
2016-08-31 23:06:36 +0800

    refactor(model): added Syllabisubject_model.php

---
+++ application/models/api/Syllabisubject_model.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Syllabisubject_model extends CI_Model {
+
+  function __construct() {
+    parent::__construct();
+  }
+
+  /**
+   * Update Syllabi's Subject(s)
+   *
+   * @param array $subjects     subject ids
+   * @param number $id          syllabus_id
+   */
+  public function update($subjects, $id) {
+    // Delete existing subject ids of syllabi
+    $this->db->where('syllabus_id', $id);
+    $this->db->delete('syllabus_subject');
+
+    // Add new subject ids of syllabi if any
+    if(count($subjects) > 0) {
+      $insertDataBatch = array();
+      foreach($subjects as $v) {
+        $insertDataBatch[] = array(
+          'syllabus_id' => $id,
+          'subject_id' => $v
+        );
+      }
+      $this->db->insert_batch('syllabus_subject', $insertDataBatch);
+    }
+    return;
+  }
+}
